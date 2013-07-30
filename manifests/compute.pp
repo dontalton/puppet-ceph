26d73ceb5013e02e9c318d5e6016656af2ad14a0
dotalton <dotalton@cisco.com>
2013-07-30 08:49:07 -0700

    package installation and secret generation

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -8,20 +8,34 @@ class ceph::compute(
 
   $volume_keyname = "client.${keyname}"
 
-  file { 'compute secret':
+  class { 'ceph::apt::ceph': release => $::ceph_release }  
+
+  package { 'ceph-common':
+    ensure => present,
+    require => Apt::Source['ceph'],
+  }
+
+  package { 'python-ceph':
+    ensure => present,
+    require => Apt::Source['ceph'],
+  }
+
+  file { '/etc/ceph/secret.xml':
     content => template('secret.xml-compute.erb'),
+    require => Package['ceph-common'],
   }
 
   exec { 'get-or-set volumes key':
     command => "ceph auth get-or-create $volume_keyname mon 'allow r' osd \
-      'allow class-read object_prefix rbd_children, allow rwx pool=${poolname}"
+      'allow class-read object_prefix rbd_children, allow rwx pool=${poolname}",
+    require => Package['ceph-common'],
   }
 
   exec { 'get-or-set virsh secret':
     command => "virsh secret-define --file secret.xml | awk '{print $2}' | \
       sed '/^$/d' > /etc/ceph/virsh.secret",
     onlyif  => 'test ! -f /etc/ceph/virsh.secret',
-    require => File['compute secret'],
+    require => File['/etc/ceph/secret.xml'],
   }
 
   exec { 'set-secret-value virsh':
