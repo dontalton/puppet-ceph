31e00b5bd49683977c9e0616f968400f1523ed3f
Donald Talton <dotalton@cisco.com>
2013-07-31 00:15:53 -0400

    compute class modifications

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -1,14 +1,13 @@
-
-
-
 class ceph::compute(
-  keyname  = 'volumes',
-  poolname = 'volumes',
+  $poolname = 'volumes',
+  $fsid = $::ceph_monitor_fsid,
 ) {
 
-  $volume_keyname = "client.${keyname}"
+  include 'ceph::package'
 
-  class { 'ceph::apt::ceph': release => $::ceph_release }  
+  Package['ceph'] -> Ceph::Key <<| title == 'admin' |>>
+
+  class { 'ceph::apt::ceph': release => $::ceph_release }
 
   package { 'ceph-common':
     ensure => present,
@@ -20,28 +19,30 @@ class ceph::compute(
     require => Apt::Source['ceph'],
   }
 
+  class { 'ceph::conf':
+    fsid => $fsid,
+  }
+
   file { '/etc/ceph/secret.xml':
-    content => template('secret.xml-compute.erb'),
+    content => template('ceph/secret.xml-compute.erb'),
     require => Package['ceph-common'],
   }
 
   exec { 'get-or-set volumes key':
-    command => "ceph auth get-or-create $volume_keyname mon 'allow r' osd \
-      'allow class-read object_prefix rbd_children, allow rwx pool=${poolname}",
+    command => "ceph auth get-or-create client.volumes mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=${poolname}' > /etc/ceph/client.volumes",
     require => Package['ceph-common'],
   }
 
   exec { 'get-or-set virsh secret':
-    command => "virsh secret-define --file secret.xml | awk '{print $2}' | \
-      sed '/^$/d' > /etc/ceph/virsh.secret",
+    command => "virsh secret-define --file secret.xml | awk '{print $2}' | sed '/^$/d' > /etc/ceph/virsh.secret",
     onlyif  => 'test ! -f /etc/ceph/virsh.secret',
     require => File['/etc/ceph/secret.xml'],
   }
 
   exec { 'set-secret-value virsh':
-    command => "virsh secret-set-value --secret $(cat /etc/ceph/virsh.secret) \
-      --base64 $(cat /etc/ceph/$volume_keyname",
+    command => "virsh secret-set-value --secret $(cat /etc/ceph/virsh.secret) --base64 $(ceph auth get-key client.volumes)",
     require => Exec['get-or-set virsh secret'],
   }
 
 }
+
